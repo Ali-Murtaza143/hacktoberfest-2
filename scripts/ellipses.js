364b2e512f0bea5f7bf5b0a1cd44fa727b7ed47a
Ali Murtaza <58934980+Ali-Murtaza143@users.noreply.github.com>
2020-10-19 19:15:25 +0500

    Making  ellipse using different methods
    using Function

--- scripts/ellipses.js
+++ scripts/ellipses.js
@@ -6,3 +6,23 @@ function draw() {
   fill(Math.floor((Math.random() * 255) + 1));
   ellipse(mouseX, mouseY, 80, 80);
 }
+
+
+function draw() {
+  fill(Math.floor((Math.random() * 215) + 1));
+  ellipse(mouseX, mouseY, 90, 90);
+}
+function draw() {
+  fill(Math.floor((Math.random() * 315) + 1));
+  ellipse(mouseX, mouseY, 100, 100);
+}
+
+
+function draw() {
+  fill(Math.floor((Math.random() * 355) + 1));
+  ellipse(mouseX, mouseY, 110, 110);
+}
+function draw() {
+  fill(Math.floor((Math.random() * 380) + 1));
+  ellipse(mouseX, mouseY, 120, 120);
+}
